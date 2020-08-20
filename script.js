4155e047325684cdb9bfd1f0b75ca5bfa672f083
Marco72RJ <marco72rj@gmail.com>
2020-08-20 14:09:22 -0300

    cobrinha 2

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 let canvas = document.getElementById("cobra");
 let context = canvas.getContext("2d");
 let box = 32;
+let cobra = [];
+cobra[0] = {
+x: 8 * box,
+y: 8 * box
+}
 
 function criarBG() {
-context.fillstyle = "lightgreen";
+context.fillStyle = "lightgreen";
 context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-criarBG()
+function criarCobrinha(){
+    for(i=0; i < cobra.length; i++){
+        context.fillStyle = "green";
+        context.fillRect(cobra[i].x, cobra[i].y, box, box);
+    }
+}
+
+criarBG();
+criarCobrinha();
