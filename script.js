5450651920ddd5a51d613cd2625127c2bdb9b595
Marco72RJ <marco72rj@gmail.com>
2020-08-21 00:07:24 -0300

    cobra3

--- script.js
+++ script.js
@@ -21,7 +21,20 @@ function criarCobrinha(){
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update (event){
+if(event.keyCode ==37 && joystick != "right") joystick = "left";
+if(event.keyCode ==38 && joystick != "down") joystick = "up";
+if(event.keyCode ==39 && joystick != "left") joystick = "right";
+if(event.keyCode ==40 && joystick != "up") joystick = "down";
+}
+
 function iniciarJogo(){
+if(cobra[0].x > 15 * box  && joystick == "right") cobra[0].x = 0;
+if(cobra[0].x < 0 && joystick == "left") cobra[0].x = 16 * box;
+if(cobra[0].y > 15 * box  && joystick == "down") cobra[0].y = 0;
+if(cobra[0].y < 0 && joystick == "up") cobra[0].y = 16 * box;
 
     criarBG();
     criarCobrinha();
